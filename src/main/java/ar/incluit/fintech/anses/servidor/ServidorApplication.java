01258ede8724d7ce74edec691bbb3b1c0508086a
matias <carossomn@gmail.com>
2019-12-10 20:43:25 -0300

    fix para compilar war

--- src/main/java/ar/incluit/fintech/anses/servidor/ServidorApplication.java
+++ src/main/java/ar/incluit/fintech/anses/servidor/ServidorApplication.java
@@ -4,13 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @ServletComponentScan
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class ServidorApplication {
+public class ServidorApplication  extends SpringBootServletInitializer {
+	
 
     public static void main(String[] args) {
         SpringApplication.run(ServidorApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServidorApplication.class);
+    }
 }
+
+
